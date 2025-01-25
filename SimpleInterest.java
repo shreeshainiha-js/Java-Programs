6504e55aaf7bf55b8d40c15a34d5588e0e9e6942
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-25 20:26:48 +0530

    Update SimpleInterest.java
    Java program to calculate the simple interest and total amount

--- SimpleInterest.java
+++ SimpleInterest.java
@@ -13,9 +13,19 @@ public class SimpleInterest {
         System.out.print("Enter the time (in years): ");
         double time = scanner.nextDouble();
 
-        double interest = (principal * rate * time) / 100;
+        double interest = (principal * rate * time) / 100;//calculate by formula of simple interest
 
         System.out.println("The simple interest is: " + interest);
-        System.out.println("The total amount is: " + (principal + interest));
+        System.out.println("The total amount is: " + (principal + interest));//add principal and interest to calculate total amount
     }
 }
+
+/* Output
+
+Enter the principal amount: 12000
+Enter the annual interest rate (in %): 4
+Enter the time (in years): 3
+The simple interest is: 1440.0
+The total amount is: 13440.0
+
+*/
