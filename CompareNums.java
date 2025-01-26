1bd6f8c487c3ab7f23f84416314824824578a310
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-26 20:58:59 +0530

    Add files via upload

---
+++ CompareNums.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CompareNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter the first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("\nComparison Results:");
+        System.out.println(num1 + " > " + num2 + " is " + (num1 > num2));
+        System.out.println(num1 + " >= " + num2 + " is " + (num1 >= num2));
+        System.out.println(num1 + " < " + num2 + " is " + (num1 < num2));
+        System.out.println(num1 + " <= " + num2 + " is " + (num1 <= num2));
+        System.out.println(num1 + " == " + num2 + " is " + (num1 == num2));
+        System.out.println(num1 + " != " + num2 + " is " + (num1 != num2));
+        scanner.close();
+    }
+}
