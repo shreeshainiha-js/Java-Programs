eea0feec3c758ea406fbb684560486ffc4f45fb5
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-26 20:50:55 +0530

    Add files via upload

---
+++ CompareTwoNums.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class CompareTwoNums {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+	System.out.print("Enter the first number: ");
+        double number1 = scanner.nextDouble();
+        System.out.print("Enter the second number: ");
+        double number2 = scanner.nextDouble();
+        if (number1 > number2) {
+            System.out.println("The first number (" + number1 + ") is greater.");
+        } else if (number2 > number1) {
+            System.out.println("The second number (" + number2 + ") is greater.");
+        } else {
+            System.out.println("Both numbers are equal.");
+        }
+
+        scanner.close();
+    }
+}
