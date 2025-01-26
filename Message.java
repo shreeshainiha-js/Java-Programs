63d91e575140ac9c3e9a59e388d638698563c0cc
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-26 21:13:23 +0530

    Update Message.java
    If a number divisible by only 4 but not 3 print "Good afternoon", else if a number divided by only 3 but not 4, print "Good evening",, else "Good morning"

--- Message.java
+++ Message.java
@@ -5,11 +5,23 @@ class Message{
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter the number:");
 		int num=sc.nextInt();
-		if(num%4==0 && num%3!=0)
+		if(num%4==0 && num%3!=0)//if a number divisible only by 4 but not 3
 			System.out.println("Good afternoon");
-		else if(num%3==0 && num%4!=0)
+		else if(num%3==0 && num%4!=0)//if a number divisible only by 3 but not 4
 			System.out.println("Good evening");
 		else
 			System.out.println("Good morning");
 	}
 }
+
+/* Output
+Enter the number:
+24
+Good morning	
+Enter the number:
+33
+Good evening
+Enter the number:
+4
+Good afternoon
+*/
