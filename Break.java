82b79721ba9de37dcb32890bf25162f9f928f3ca
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-28 19:37:27 +0530

    Update Break.java

--- Break.java
+++ Break.java
@@ -6,9 +6,17 @@ int number=0;
 while(number<10)
 {
 number=number+1;
-if(number==5)
+if(number==5)// check if the number is equal to 5 and if its true the loop terminates at this iteration.
 break;
-System.out.println(number);
+System.out.println(number);// To print the number upto 4 (until the break condition is reached)
 }
 System.out.println("After break")
 }}
+
+/* Output
+1
+2
+3
+4
+After break
+*/
