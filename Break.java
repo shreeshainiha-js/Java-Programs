d37edb992e55cbef61d2ffcd60f21506a3508f66
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-28 19:34:37 +0530

    Add files via upload
    Java program to print numbers using break statement.

---
+++ Break.java
@@ -0,0 +1,14 @@
+class Break
+{
+public static void main(String [] args)
+{
+int number=0;
+while(number<10)
+{
+number=number+1;
+if(number==5)
+break;
+System.out.println(number);
+}
+System.out.println("After break")
+}}
