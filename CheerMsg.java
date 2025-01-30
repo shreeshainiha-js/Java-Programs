f5dd433111260a67a5cec80d2c3b62991eeb899a
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-30 21:20:47 +0530

    Add files via upload
    Imagine you need to repeat a cheerful message. Write a program that uses a for loop to print "ALL IS WELL" exactly twenty times.

---
+++ CheerMsg.java
@@ -0,0 +1,32 @@
+class CheerMsg{
+	public static void main(String [] args)
+	{
+		for(int i=1;i<=20;i++)// set range upto 20 to print exactly 20 times.
+		{
+			System.out.println(i+ " ALL IS WELL");
+		}
+	}
+}
+
+/* Output
+1 ALL IS WELL
+2 ALL IS WELL
+3 ALL IS WELL
+4 ALL IS WELL
+5 ALL IS WELL
+6 ALL IS WELL
+7 ALL IS WELL
+8 ALL IS WELL
+9 ALL IS WELL
+10 ALL IS WELL
+11 ALL IS WELL
+12 ALL IS WELL
+13 ALL IS WELL
+14 ALL IS WELL
+15 ALL IS WELL
+16 ALL IS WELL
+17 ALL IS WELL
+18 ALL IS WELL
+19 ALL IS WELL
+20 ALL IS WELL
+*/
