5ef51abbf726dce6ff63e9f366b0a97bb1bc531a
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-30 19:07:49 +0530

    Add files via upload

---
+++ InfiniteLoops.java
@@ -0,0 +1,19 @@
+public class InfiniteLoops {
+    public static void main(String[] args) {
+        // Infinite for loop (without any exit condition.)
+        System.out.println("Starting infinite for loop...");
+        for (;;) { 
+            System.out.println("This is an infinite for loop!");
+        }
+
+        // Infinite while loop
+        System.out.println("Starting infinite while loop...");
+        while (true) {
+        System.out.println("This is an infinite while loop!");
+         }
+    }
+}
+
+
+
+//The output of the program prints the statement infinite times.
