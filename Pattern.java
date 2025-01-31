a19f74c85898e0f93f385f83e4644df3b39e5945
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-01-31 21:00:47 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,29 @@
+
+public class Pattern {
+
+	public static void main(String[] args) {
+		int n=5;
+		for(int i=0;i<n;i++) //rows 
+			{
+			for(int j=0;j<n;j++)//colums (both rows and column counts are same)
+			{
+				System.out.print("*");
+			}
+			System.out.println();	
+			System.out.print("\n");
+			}
+
+	}
+
+}
+
+/* Output
+
+*****
+*****
+*****
+*****
+*****
+*/
+
+
