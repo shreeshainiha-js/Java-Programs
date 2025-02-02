a308143f86072e377bdea390d26a275001df4425
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-02-02 19:55:03 +0530

    Create Boxnum.java

---
+++ Boxnum.java
@@ -0,0 +1,26 @@
+
+public class Box {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=0;i<=n;i++) {
+			for(int j=0;j<n;j++) {
+				System.out.print((i+1)+" ");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
+
+
+/* Output
+
+1 1 1 1 
+2 2 2 2 
+3 3 3 3 
+4 4 4 4 
+5 5 5 5 
+ */
+
