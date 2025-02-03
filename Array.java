b9b28e27b84c3e06366a7549e81ff79d8e282202
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-02-03 20:18:56 +0530

    Create Array.java
    To print elements in an array.

---
+++ Array.java
@@ -0,0 +1,19 @@
+class Main {
+    public static void main(String[] args) {
+		int []arr=new int[] {1,2,3,4,5};
+		for(int i=0;i<=4;i++) 
+		{
+			System.out.println(arr[i]);
+		}
+		
+
+    }
+}
+
+/* Output
+1
+2
+3
+4
+5
+*/
