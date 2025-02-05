104131f6301dd4ae1e65a7a3719874125b9a7181
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-02-05 18:46:51 +0530

    Create WithoutretWP.java

---
+++ WithoutretWP.java
@@ -0,0 +1,17 @@
+
+public class Worwp {
+	//without return type and with parameter
+	static void display(int a,int b) {
+		int res=a+b;
+		System.out.println(res);
+	}
+	public static void main(String[] args) {
+		int x=8;
+		int y=10;
+		display(x,y);//call the function
+
+	}
+
+}
+
+//Output:18
