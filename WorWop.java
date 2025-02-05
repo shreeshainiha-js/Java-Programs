8df1342f92816258d1cedf0d260f51c19f53c32f
Shree Shainiha JS <shreeshainiha27@gmail.com>
2025-02-05 18:47:56 +0530

    Create WorWop.java

---
+++ WorWop.java
@@ -0,0 +1,17 @@
+
+public class WorWop {
+
+	static void display() //without return type and without parameter
+	{
+		System.out.println("Hello World");
+	}
+
+	public static void main(String[] args) {
+		display();
+		
+
+	}
+
+}
+
+//Output: Hello World
